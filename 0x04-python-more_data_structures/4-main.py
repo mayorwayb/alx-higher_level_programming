fb9b10cb9031b8e827a46c478e5dafc99ef0a139
mayorwayb <mayorwayb@gmail.com>
2023-12-12 14:36:26 -0800

    4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-main.py
@@ -0,0 +1,8 @@
+guillaume@ubuntu:~/0x04$ cat 4-main.py
+#!/usr/bin/python3
+only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
