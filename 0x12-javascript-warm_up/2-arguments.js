902ca5f66a239ce5b0e50e3ab8690dde892ef1de
mayorwayb <mayorwayb@gmail.com>
2024-06-21 01:23:49 -0700

    update 1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-const args = process.argv.slice(2);
+const count = process.argv.length;
 
-if (args.length === 0) {
-    console.log("No argument");
-} else if (args.length === 1) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
+console.log(
+    count === 2 ? 'No argument' :
+    count === 3 ? 'Argument found' :
+    'Arguments found'
+);
+
